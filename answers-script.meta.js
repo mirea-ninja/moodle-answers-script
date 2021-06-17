4b2db83c12dce6759158e5cb75bc8ea8667fb677
SyntOwl <79212038+SyntOwl@users.noreply.github.com>
2021-06-17 20:37:42 +0300

    Add update info

---
+++ answers-script.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Mirea Ninja Answers
+// @namespace    https://mirea.ninja/
+// @version      1.1.5
+// @description  online test answers!
+// @author       admin
+// @match        *://*/*
+// @updateURL    https://raw.githubusercontent.com/Ninja-Official/answers-script/main/release.js
+// @downloadURL  https://raw.githubusercontent.com/Ninja-Official/answers-script/main/release.js
+// @supportURL   https://mirea.ninja/t/novaya-versiya-skripta-dlya-obmena-otvetami-v-testirovaniya-v-sdo/486
+// @require      https://cdn.jsdelivr.net/npm/@fingerprintjs/fingerprintjs@3/dist/fp.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js
+// @require      https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js
+// @require      https://cdn.socket.io/4.1.2/socket.io.min.js
+// @grant        GM_addStyle
+// @run-at       document-start
+// ==/UserScript==
