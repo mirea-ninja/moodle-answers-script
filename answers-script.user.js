cc8357ad4b80c5ed21b5faecfc079b6bdd3523a6
SyntOwl <79212038+SyntOwl@users.noreply.github.com>
2021-06-18 08:10:46 +0300

    Add name implementation for class User

--- answers-script.user.js
+++ answers-script.user.js
@@ -21,6 +21,35 @@
 
     class User {
 
+        listName = ['Крофос',
+            'Соми',
+            'Афионаполис',
+            'Костолом',
+            'Райнор',
+            'Фэктори',
+            'Фокос',
+            'Чармондер',
+            'Самордер',
+            'Рока',
+            'Зонт',
+            'Кустик',
+            'Хромус',
+            'Харивер',
+            'Шильма',
+            'Нискофор',
+            'Разор',
+            'Миру',
+            'Кросун',
+            'Сивилиус',
+            'Протон',
+            'Слардар',
+            'Дум'];
+
+        /**
+         * @private
+         * @type {string}
+         */
+        _userId = undefined;
         get UserId() {
             if (!this._userId) {
                 console.error('User Id not set.');
@@ -30,6 +59,11 @@
 
         set UserId(newId) {
             this._userId = newId;
+        }
+
+        get UserName() {
+            let codeLastSymbolUserId = this._userId.charCodeAt(this.UserId.length - 1);
+            return (this.listName)[codeLastSymbolUserId % (this.listName.length - 1)];
         }
     }
 
