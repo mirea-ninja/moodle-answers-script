676a0207abe1031be8b5e9f7f53abc0c8c0f2fa6
SyntOwl <79212038+SyntOwl@users.noreply.github.com>
2021-06-19 14:37:05 +0300

    Removing unused dependencies

--- answers-script.user.js
+++ answers-script.user.js
@@ -11,7 +11,6 @@
 // @supportURL   https://mirea.ninja/t/novaya-versiya-skripta-dlya-obmena-otvetami-v-testirovaniya-v-sdo/486
 // @require      https://cdn.jsdelivr.net/npm/@fingerprintjs/fingerprintjs@3/dist/fp.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js
-// @require      https://ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js
 // @require      https://cdn.socket.io/4.1.2/socket.io.min.js
 // @grant        GM_addStyle
 // @run-at       document-start
