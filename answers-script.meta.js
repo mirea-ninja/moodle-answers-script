76cec4f6d74716a6b89e57197c8ebc2385cf933c
Oniel <51058739+0niel@users.noreply.github.com>
2021-06-19 16:34:47 +0300

    Update answers-script.meta.js

--- answers-script.meta.js
+++ answers-script.meta.js
@@ -6,8 +6,8 @@
 // @author       admin and SyntOwl
 // @match        *://online-edu.mirea.ru/*
 // @match        *://oniel.beget.tech/*
-// @updateURL    https://raw.githubusercontent.com/SyntOwl/answers-script/main/answers-script.meta.js
-// @downloadURL  https://raw.githubusercontent.com/SyntOwl/answers-script/main/answers-script.user.js
+// @updateURL    https://raw.githubusercontent.com/Ninja-Official/moodle-answers-script/main/answers-script.meta.js
+// @downloadURL  https://raw.githubusercontent.com/Ninja-Official/moodle-answers-script/main/answers-script.user.js
 // @supportURL   https://mirea.ninja/t/novaya-versiya-skripta-dlya-obmena-otvetami-v-testirovaniya-v-sdo/486
 // @require      https://cdn.jsdelivr.net/npm/@fingerprintjs/fingerprintjs@3/dist/fp.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js
