bd68ba98637c2838689544f612e6a739204f3127
SyntOwl <79212038+SyntOwl@users.noreply.github.com>
2021-06-19 14:26:04 +0300

    Add Hint class

--- answers-script.user.js
+++ answers-script.user.js
@@ -525,6 +525,39 @@
         }
     }
 
+    class Hint {
+
+        _domHintBlock;
+
+        /**
+         * @callback
+            */
+        callBackPressApprovalButton;
+
+        /**
+         * @param {HTMLElement}answerBlock
+         * @param {function}callBackGetAnswer
+         * @param {function}callBackGetQuestion
+         */
+        constructor(answerBlock, callBackGetAnswer, callBackGetQuestion) {
+            this._domAnswerBlock = answerBlock;
+            this._callBackGetAnswer = callBackGetAnswer;
+            this._callBackGetQuestion = callBackGetQuestion;
+        }
+
+        /**
+         * @abstract
+         */
+        set HintInfo(info) {
+
+        }
+
+        /**
+         * @abstract
+         */
+        CreateHint() {
+        }
+    }
     /**
      * @abstract
      */
