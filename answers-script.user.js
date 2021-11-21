6831b31fd4ee521b2e65476dd4652505fe6368c9
SyntOwl <79212038+SyntOwl@users.noreply.github.com>
2021-11-21 20:04:36 +0300

    Upgrade CryptoJS version

--- answers-script.user.js
+++ answers-script.user.js
@@ -11,7 +11,7 @@
 // @downloadURL https://raw.githubusercontent.com/Ninja-Official/moodle-answers-script/main/answers-script.user.js
 // @supportURL  https://mirea.ninja/t/novaya-versiya-skripta-dlya-obmena-otvetami-v-testirovaniya-v-sdo/486
 // @require     https://cdn.jsdelivr.net/npm/@fingerprintjs/fingerprintjs@3/dist/fp.min.js
-// @require     https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.0.0/crypto-js.min.js
+// @require     https://cdnjs.cloudflare.com/ajax/libs/crypto-js/4.1.1/crypto-js.min.js
 // @require     https://cdn.socket.io/4.1.2/socket.io.min.js
 // @run-at      document-start
 // @grant       GM_addStyle
