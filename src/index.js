c0735b320732f688b32e2e512b3b97bb6481f05e
SyntOwl <79212038+SyntOwl@users.noreply.github.com>
2021-12-22 22:42:45 +0300

    Fix: DI window

--- src/index.js
+++ src/index.js
@@ -19,7 +19,9 @@ function OnDOMReady() {
                 switch (document.readyState) {
                 case 'loading':
                 case 'interactive':
-                    window.addEventListener('load', app.Start);
+                    window.addEventListener('load', function () {
+                        app.Start();
+                    });
                     break;
                 case 'complete':
                     app.Start();
